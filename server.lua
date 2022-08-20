3ad22c051c11680fd96b58475964dfcc333a1c81
Lama <79053058+ItzEndah@users.noreply.github.com>
2022-08-20 23:15:51 +0200

    Add files via upload

---
+++ server.lua
@@ -0,0 +1,35 @@
+-- ND_Framework exports
+NDCore = exports["ND_Core"]:GetCoreObject()
+
+RegisterServerEvent("LamasJobs:GivePay")
+AddEventHandler("LamasJobs:GivePay", function(amount)
+	-- to do: validate amount from client to avoid exploit
+    NDCore.Functions.AddMoney(amount, source, "bank")
+end)
+
+RegisterServerEvent("LamasJobs:GivePenalty")
+AddEventHandler("LamasJobs:GivePenalty", function(amount)
+    NDCore.Functions.DeductMoney(amount, source, "bank")
+end)
+
+-- version checker
+Citizen.CreateThread( function()
+	updatePath = "/ItzEndah/TruckJob"
+	resourceName = "TruckJob by Lama"
+	
+	function checkVersion(err, responseText, headers)
+		-- Returns the version set in the file
+		curVersion = GetResourceMetadata(GetCurrentResourceName(), "version")
+
+		if responseText == nil or curVersion == nil then
+			print("^1There was an error retrieving the version of " .. resourceName .. ": the version checker will be skipped.")
+		else if tonumber(curVersion) == tonumber(responseText) then
+			print("^2" .. resourceName .." is up to date. Enjoy.")
+		else 
+			print("^1" .. resourceName .. " is outdated.\nLatest version: " .. responseText .. "Current version: " .. curVersion .. "\nPlease update it from: https://github.com" .. updatePath)
+		end
+	end
+end
+	
+	PerformHttpRequest("https://raw.githubusercontent.com" .. updatePath .. "/main/version", checkVersion, "GET")
+end)
